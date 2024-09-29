7f92294e9a2c71b469747c5fdb2f1b8f4fc20356
Raden-nvii <navinugraha15@gmail.com>
2024-09-29 13:58:00 +0800

    fix(not done): product catalog

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import HomePage from "@/pages/HomePage";
 import ProductDetail from "../pages/ProductPage";
 import BaseLayout from "../components/ui/Baselayout";
+import { Product } from "@/models/Product";
+
+async function productsLoader(): Promise<Product[]> {
+  const response = await fetch(
+    "https://homeserver-backend.navingrh.com/api/products"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return response.json();
+}
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +22,8 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+        loader: productsLoader,
+        // errorElement: <ErrorBoundary />,
       },
       {
         path: "/products/:id",
@@ -19,3 +32,13 @@ export const router = createBrowserRouter([
     ],
   },
 ]);
+
+function ErrorBoundary() {
+  return (
+    <div className="text-center text-red-500">
+      An error occurred. Please try again later.
+    </div>
+  );
+}
+
+export default router;
